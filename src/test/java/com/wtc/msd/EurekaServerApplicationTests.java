093c53e881079fd357ecf688ee51ae377b506072
imran <imran@i3book.dot>
2019-03-11 22:17:39 +0000

    Renamed test for server. Moved package.

--- src/test/java/com/wtc/msd/EurekaServerApplicationTests.java
+++ src/test/java/com/wtc/msd/EurekaServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.wtc.msd.msdeurekasvr;
+package com.wtc.msd;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,10 +7,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class DemoApplicationTests {
+public class EurekaServerApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+  @Test
+  public void contextLoads() {
+  }
 
 }
